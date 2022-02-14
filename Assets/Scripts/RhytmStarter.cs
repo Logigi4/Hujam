9cc50dbca841282bf9caf7308ffe7cff7e2ec2b8
Arda Akdeniz <ardaakdeniizz@gmail.com>
2022-02-14 11:35:18 +0300

    A.2

--- Assets/Scripts/RhytmStarter.cs
+++ Assets/Scripts/RhytmStarter.cs
@@ -79,7 +79,7 @@ public class RhytmStarter : MonoBehaviour
             }
         }
 
-        MultiText.text = CurrentMultiplier + "X";
+        MultiText.text = "Combo: " + CurrentMultiplier + "X";
 
        // CurrentScore += ScorePerNote * CurrentMultiplier;
        scoreText.text = "Score: " + CurrentScore;
@@ -108,7 +108,7 @@ public class RhytmStarter : MonoBehaviour
         Debug.Log("Miss");
         CurrentMultiplier = 1;
         MultiplierTracker = 0;
-        MultiText.text = CurrentMultiplier + "X";
+        MultiText.text = "Combo: " + CurrentMultiplier + "X";
     }
 
 
